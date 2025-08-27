bfcd957e66e5958537574a03593d8a26474cd065
Capdavid777 <162670566+Capdavid777@users.noreply.github.com>
2025-08-27 11:11:43 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
 // pages/_app.js
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import '../styles/globals.css';
-import Header from '../components/Header';
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
-      <Header />
-      {/* keep content below the sticky header */}
-      <main className="pt-14">
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </UserProvider>
   );
 }
