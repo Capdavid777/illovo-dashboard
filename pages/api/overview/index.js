1de18a8332daf954fb112aad638bd36c28b4bce0
Capdavid777 <162670566+Capdavid777@users.noreply.github.com>
2025-09-08 09:47:48 +0200

    Update index.js

--- pages/api/overview/index.js
+++ pages/api/overview/index.js
@@ -188,7 +188,7 @@ export default async function handler(req, res) {
       );
 
     // top-level + totals (for compatibility)
-    return res.status(200).json({
+    return res.status(200).json({ month,
       ok: true,
       revenueToDate: revSum,
       targetToDate: tgtSum,
